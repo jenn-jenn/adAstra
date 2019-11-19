199f4e47d2f562a3839503ead6a877e34e86f42f
Joanna Chen <x.chenj@gmail.com>
2019-11-19 14:21:38 -0800

    Final push before master merge

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -8,7 +8,6 @@ import SplashPage from './splash/splash_page';
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
-import MapContainer from './map/map_container';
 
 const App = () => (
   <div>
@@ -17,7 +16,6 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-      <AuthRoute exact path="/map" component={MapContainer} />
       <ProtectedRoute exact path="/splash" component={SplashPage} />
     </Switch>
   </div>
