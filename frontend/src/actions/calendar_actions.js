25df732d367274f1052d42af00dad07c6c2678c8
zhenjess <jessicazhen8@gmail.com>
2019-11-19 10:47:48 -0800

    Add calendar actions

---
+++ frontend/src/actions/calendar_actions.js
@@ -0,0 +1,45 @@
+import * as APIUtil from "../util/calendar_util";
+
+export const RECEIVE_ALL_DATES = 'RECEIVE_ALL_DATES';
+export const RECEIVE_DATE = 'RECEIVE_DATE';
+export const REMOVE_DATE = 'REMOVE_DATE';
+
+export const receiveAllDates = dates => ({
+    type: RECEIVE_ALL_DATES, 
+    dates
+});
+
+export const receiveDate = date => ({
+    type: RECEIVE_DATE, 
+    date
+});
+
+export const removeDate = date => ({
+    type: REMOVE_DATE, 
+    date
+});
+
+export const createDate = (date) => dispatch => (
+    APIUtil.createDate(date) 
+        .then(date => dispatch(receiveDate(date)))
+);
+
+export const fetchAllDates = (userId) => dispatch => (
+    APIUtil.fetchAllDates(userId)
+        .then(dates => dispatch(receiveAllDates(dates)))
+);
+
+export const fetchDate = (dateId) => dispatch => (
+    APIUtil.fetchDate(dateId)
+        .then(date => dispatch(receiveDate(date)))
+);
+
+export const editDate = (dateId) => dispatch => (
+    APIUtil.editDate(dateId)
+        .then(date => dispatch(receiveDate(date)))
+);
+
+export const deleteDate = (dateId) => dispatch => (
+    APIUtil.deleteDate(dateId)
+        .then(dateId => dispatch(removeDate(dateId)))
+);
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import session from "./session_api_reducer";
 import errors from "./errors_reducer";
+import calendar from "./calendar_reducer";
 
 const RootReducer = combineReducers({
   errors,
   session,
+  calendar
 });
 
 export default RootReducer;
