c0393b704dbb1ee0b27248469dbf031281c5bda0
zhenjess <jessicazhen8@gmail.com>
2019-11-19 10:23:20 -0800

    Create calendar api util

---
+++ frontend/src/util/calendar_util.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const createDate = date => {
+    return axios.post('/api/events/', date);
+};
+
+export const fetchAllDates = (userId) => {
+    return axios.get(`/api/events/user/${userId}`);
+};
+
+export const fetchDate = (dateId) => {
+    return axios.get(`/api/events/${dateId}`);
+};
+
+export const editDate = (dateId, date) => {
+    return axios.patch(`/api/events/${dateId}`, date);
+};
+
+export const deleteDate = (dateId) => {
+    return axios.delete(`/api/events/${dateId}`);
+};
