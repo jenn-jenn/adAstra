81675229d8d69740503217b0a1500d25483dcf2e
Joanna Chen <x.chenj@gmail.com>
2019-11-19 14:19:47 -0800

    Style map marker popups

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -10,21 +10,20 @@ class Map extends React.Component {
     this.props.fetchCosmicObjects()
     .then(() => {
       mapboxgl.accessToken = ApiKeys.mapboxAccessToken;
-
+      let currCenter = [-98.5795, 39.8283];
       const mapOptions = {
         container: "map",
-        minZoom: 11.5,
-        center: [-122.4194, 37.7749],
+        minZoom: 2.5,
+        center: currCenter,
+        pitch: 50,
         // style: 'mapbox://styles/mapbox/navigation-preview-night-v2'
-        style: 'mapbox://styles/mapbox/navigation-guidance-night-v2'
-        // style: 'mapbox://styles/mapbox/dark-v9'
+        style: "mapbox://styles/mapbox/navigation-guidance-night-v2"
       };
 
       let mapbox;
       mapbox = new mapboxgl.Map(mapOptions);
 
       const markerPlaces = this.props.cosmicObjects;
-      console.log(markerPlaces)
 
       this.MarkerManager = new MarkerManager(mapbox);
       this.MarkerManager.updateMarkers(markerPlaces);
@@ -37,18 +36,43 @@ class Map extends React.Component {
           trackUserLocation: true
         })
       );
-    })
 
+      mapbox.addControl(new mapboxgl.NavigationControl());
+
+      const coordinates = document.getElementById("coordinates");
+      const marker = new mapboxgl.Marker({
+        draggable: true
+      })
+        .setLngLat([-98.5795, 39.8283])
+        .addTo(mapbox);
+
+      function onDragEnd() {
+        const lngLat = marker.getLngLat();
+        coordinates.style.display = "block";
+        coordinates.innerHTML =
+          "Longitude: " + lngLat.lng + "<br />Latitude: " + lngLat.lat;
+      }
+
+      marker.on("dragend", onDragEnd);
+      currCenter = [marker._lngLat.lng, marker._lngLat.lat]
+    })
   }
 
-
   render() {
-    if (this.props.cosmicObjects.length === 0) return null;
+    // if (this.props.cosmicObjects.length === 0) return null;
     return (
       <div id="map-container">
-        <div id='map'></div>
+        <div id="map"></div>
+        <div className="map-overlay">
+          <input
+            id="feature-filter"
+            type="text"
+            placeholder="Search for a star..."
+          />
+        </div>
+        <pre id="coordinates" className="coordinates"></pre>
       </div>
-    )
+    );
   }
 }
 
--- frontend/src/components/map/map_marker_manager.js
+++ frontend/src/components/map/map_marker_manager.js
@@ -4,24 +4,35 @@ class MarkerManager {
   constructor(map) {
     this.map = map;
     this.updateMarkers = this.updateMarkers.bind(this);
-    this.createMarkerFromObject = this.createMarkerFromObject.bind(this);
+    this.createMarker = this.createMarker.bind(this);
   }
 
-  createMarkerFromObject(object) {
+  createMarker(object) {
     const el = document.createElement('div');
     el.className = 'marker';
 
     const lng = object.ra.decimal;
     const lat = object.dec.decimal;
-    const coordinates = [lng, lat]    
+    const coordinates = [lng, lat];
+
+    const popup = new mapboxgl.Popup({
+      offset: 25,
+      className: "popup"
+    })
+      .setLngLat(coordinates)
+      .setHTML(`<h1>${object.target.name}</h1>`)
+      // .setMaxWidth("100px")
+      .addTo(this.map);
+
 
     new mapboxgl.Marker(el)
       .setLngLat(coordinates)
-      .addTo(this.map);
+      .addTo(this.map)
+      .setPopup(popup)
   }
 
   updateMarkers(objects) {
-    objects.map(object => (this.createMarkerFromObject(object)));
+    objects.map(object => (this.createMarker(object)));
   }
 }
 
