a67c4890be277a43a4be1cd277113a3e467e74a9
Albert Yee <albertyee84@gmail.com>
2019-11-21 13:45:53 -0800

    add icons to weather

--- frontend/src/components/weather/weather_item.jsx
+++ frontend/src/components/weather/weather_item.jsx
@@ -5,13 +5,13 @@ class WeatherItem extends React.Component{
     render(){
         return (
           <div className="Days">
+            <img src={this.props.day.icon} alt=""/>
             <div>{this.props.day.name}</div>
             <div>
               {this.props.day.temperature}
               {this.props.day.temperatureUnit}
             </div>
             <div>{this.props.day.shortForecast}</div>
-            {/* <div>{this.props.day.detailedForecast}</div> */}
             <div>Winds: {this.props.day.windSpeed}</div>
           </div>
         );
