16eba8ba4d6b7b5a273e7fa02932d6a84797a791
zhenjess <jessicazhen8@gmail.com>
2019-11-20 19:32:16 -0800

    Add events reducer

--- frontend/src/reducers/events_reducer.js
+++ frontend/src/reducers/events_reducer.js
@@ -1,5 +1,6 @@
 import {
     RECEIVE_EVENTS,
+    RECEIVE_DATE_EVENTS,
     RECEIVE_NEW_EVENT,
     RECEIVE_AN_EVENT,
     REMOVE_EVENT
@@ -16,6 +17,21 @@ const EventsReducer = (
             case RECEIVE_EVENTS:
                 newState.all = action.events.data;
                 return newState;
-            case RECEIVE_
+            case RECEIVE_DATE_EVENTS:
+                newState.date = action.events.data;
+                return newState;
+            case RECEIVE_NEW_EVENT:
+                newState.date.push(action.event.data);
+                return newState;
+            case RECEIVE_AN_EVENT:
+                newState[action.event.data.id] = action.event.data;
+                return newState;
+            case REMOVE_EVENT:
+                delete newState[action.event.data.id];
+                return newState;
+            default:
+                return oldState;
         }
-    }
+    };
+
+    export default EventsReducer;
